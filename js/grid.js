8d84a144c64c68c371e4f32cba808e420d0d7f25
Md-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2021-02-19 03:08:02 +0600

    update animation and responsive

--- js/grid.js
+++ js/grid.js
@@ -321,7 +321,7 @@ var Grid = (function() {
 			// create Preview structure:
 			this.$title = $( '<h4></h4>' );
 			this.$description = $( '<p></p>' );
-			this.$href = $( '<a href="#" class="btn btn-theme">Site URL</a>' );
+			this.$href = $( '<a href="#" target="_blank" class="btn btn-theme">Site URL</a>' );
 			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$href );
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
